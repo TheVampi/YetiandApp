03b05668c6e0cb6d69de29e5e02e92592c67c5b8
The Vampi <luisitorespawn@gmail.com>
2022-06-14 21:10:53 -0500

    Funcion de universidad lista para utilizar

--- yetiand_app/lib/screens/home_page.dart
+++ yetiand_app/lib/screens/home_page.dart
@@ -16,30 +16,55 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             Container(
-              margin: const EdgeInsets.fromLTRB(7.0, 5.0, 7.0, 5.0),
-              child: Container(
-                child: const Text('Tecno',
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
-              ),
-            ),
-            Container(
-              child: Row(
-                children: const [
-                  Image(
-                    image: AssetImage('assets/tecno_logo.jpg'),
-                    width: 100,
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(7.0, 5.0, 7.0, 5.0),
+                    child: Container(
+                      child: const Text('Instituto Tecnologico de Celaya',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20)),
+                    ),
                   ),
-                  SizedBox(
-                    width: 10.0,
+                  Container(
+                    child: Row(
+                      children: const [
+                        Image(
+                          image: AssetImage('assets/tecno_logo.jpg'),
+                          width: 100,
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Expanded(
+                          child: Text(
+                              '\n• Ing. Ambiental.\n• Ing. Bioquimica\n• Ing. Electronica\n• Ing. Gestion Empresarial\n• Sistemas Computacionales\n• Ing. Industrial\n• Ing. Mecanica\n• Ing. Mecatronica\n• Ing. Quimica\n• Lic. Administracion de Empresas'),
+                        ),
+                      ],
+                    ),
                   ),
-                  Expanded(
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  const Center(
                     child: Text(
-                        '\n• Licenciatura en Ciencias de la Comunicación.\n• Licenciatura en Relaciones Internacionales.\n• Licenciatura en Contaduría.\n• Licenciatura en Derecho.'),
+                        'AVENIDA TECNOLOGICO E IRRIGACION NUMERO EXTERIOR: SN, COLONIA ALFREDO VAZQUEZ BONFIL 4616117575'),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
                   ),
                 ],
               ),
             ),
+            //A PARTIR DE AQUI VAN LAS FUNCIONES PARA CADA UNIVERSIDAD
+            _containerUniversidad(
+                'Instituto Tecnologico de Roque',
+                'assets/roque_logo.jpg',
+                '\n• Licenciatura en Ciencias de la Comunicación.\n• Licenciatura en Relaciones Internacionales.\n• Licenciatura en Contaduría.\n• Licenciatura en Derecho.',
+                'KILOMETRO 8 NUMERO CARRETERA CELAYA-JUVENTINO ROSAS 4616115903'),
+
+            //AQUI ABAJO VAS A SUSTITUIR LOS VALORES DE ACUERDO A CADA UNIVERSIDAD
+            
           ],
         ),
       ),
@@ -55,12 +80,45 @@ class HomePage extends StatelessWidget {
   }
 }
 
-Container _containerUniversidad(String logoUni, String textoCarrera) {
+Container _containerUniversidad(String nombreUni, String logoUni,
+    String textoCarrera, String textoDomicilio) {
   return Container(
-    margin: const EdgeInsets.fromLTRB(7.0, 5.0, 7.0, 5.0),
-    child: Container(
-      child: const Text('Tecno',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
+    child: Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.fromLTRB(7.0, 5.0, 7.0, 5.0),
+          child: Container(
+            child: Text(nombreUni,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          ),
+        ),
+        Container(
+          child: Row(
+            children: [
+              Image(
+                image: AssetImage(logoUni),
+                width: 100,
+              ),
+              const SizedBox(
+                width: 10.0,
+              ),
+              Expanded(
+                child: Text(textoCarrera),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        Center(
+          child: Text(textoDomicilio),
+        ),
+        const SizedBox(
+          height: 20.0,
+        ),
+      ],
     ),
   );
 }
