b7b199cb4a66b339f57734b0204896b201566fda
DaxterGtz <c.gutierrezguerero@gmail.com>
2022-06-20 01:16:26 -0500

    Pestaña Ingeneria terminada

--- yetiand_app/lib/screens/ramasDeEstudios/ingenieria.dart
+++ yetiand_app/lib/screens/ramasDeEstudios/ingenieria.dart
@@ -155,6 +155,61 @@ class ramaIngenieria extends StatelessWidget {
                   'assets/ceib_logo.jpg',
                   '\n• Ing. Industrial Administrativa.\n• Lic. en Diseño y Desarrollo de Aplicaciones.',
                 ),
+                _containerUniversidad(
+                  'Centro Universitario Celsus',
+                  'assets/celsus_logo.png',
+                  '\n• Ing. en Biotecnología.\n• Ing. Industrial.\n• Ing. en Mecatrónica.',
+                ),
+                _containerUniversidad(
+                  'Instituto Universitario del Centro de México',
+                  'assets/educem_logo.jpg',
+                  ' \n• Ing. Industrial.\n• Ing. Mecánica Automotriz.',
+                ),
+                _containerUniversidad(
+                  'Centro Universitario Pablo Picasso',
+                  'assets/pablopicasso_logo.png',
+                  '\n• Ing. Industrial y de Sistemas ',
+                ),
+                _containerUniversidad(
+                  'Instituto Tecnológico de Roque',
+                  'assets/roque_logo.jpg',
+                  '\n• Ing. en Gestión Empresarial.\n• Ing. en Tecnologías de la Información y Comunicaciones.\n• Ing. Agrónoma.\n• Ing. en Industrias Alimentarias.\n• Ing. en Innovación Agrícola Sostenible.\n• Ing. Hidrológica.',
+                ),
+                _containerUniversidad(
+                  'Sabes Unideg Celaya',
+                  'assets/sabes_logo.png',
+                  '\n• Ing. Industrial.\n• Ing. en Logística.\n• Ing. en Tecnologías de la Información.',
+                ),
+                _containerUniversidad(
+                  'Instituto Tecnológico de Celaya',
+                  'assets/tecno_logo.jpg',
+                  '\n• Ing. Ambiental.\n• Ing. Bioquímica.\n• Ing. Electrónica.\n• Ing. en Gestion Empresarial.\n• Ing. Industrial.\n• Ing. Mecánica.\n• Ing. Mecatrónica.',
+                ),
+                _containerUniversidad(
+                  'Universidad Latina de México',
+                  'assets/ulm_logo.png',
+                  ' Ing. Industrial.',
+                ),
+                _containerUniversidad(
+                  'UNITESBA Universidad',
+                  'assets/unitesba_logo.jpg',
+                  '\n• Ing. Industrial.\n• Ing. Civil',
+                ),
+                _containerUniversidad(
+                  'UTEC Celaya - Universidad Tecnológica del Centro de México',
+                  'assets/utec_logo.png',
+                  '\n• Ing. en Diseño Industrial.\n• Ing. Industrial.',
+                ),
+                _containerUniversidad(
+                  'Universidad Tecnológica Laja Bajío',
+                  'assets/utlb_logo.png',
+                  '\n• Ing. en logística comercial global.\n• Ing. en mecatrónica.',
+                ),
+                _containerUniversidad(
+                  'Universidad Virtual del Estado de Guanajuato UVEG',
+                  'assets/uveg_logo.jpg',
+                  '\n• Ing. en Gestión de Proyectos.\n• Ing. en Gestión de Tecnologías de Información.\n• Ing. en Industrial.',
+                ),
               ],
             ),
           ),
